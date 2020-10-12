77332bacb8ff82a3a44cc550f51b000665fbfae3
MNasybullin <mansurnasybullin@gmail.com>
2020-10-12 20:47:01 +0300

    opt color.c

--- src/color.c
+++ src/color.c
@@ -6,7 +6,7 @@
 /*   By: sdiego <sdiego@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 15:32:12 by sdiego            #+#    #+#             */
-/*   Updated: 2020/10/07 18:54:56 by sdiego           ###   ########.fr       */
+/*   Updated: 2020/10/12 20:45:52 by sdiego           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,48 +24,42 @@ t_color	color(double r, double g, double b)
 
 t_color	add_col(t_color a1, t_color a2)
 {
-	t_color	b;
-
-	b.r = a1.r + a2.r;
-	b.g = a1.g + a2.g;
-	b.b = a1.b + a2.b;
-	return (b);
+	a1.r += a2.r;
+	a1.g += a2.g;
+	a1.b += a2.b;
+	return (a1);
 }
 
 t_color	sub_col(t_color a1, t_color a2)
 {
-	t_color	b;
-
-	b.r = a1.r - a2.r;
-	b.g = a1.g - a2.g;
-	b.b = a1.b - a2.b;
-	return (b);
+	a1.r -= a2.r;
+	a1.g -= a2.g;
+	a1.b -= a2.b;
+	return (a1);
 }
 
 t_color	mult_col(t_color a, double b)
 {
-	a.r = a.r * b;
-	a.g = a.g * b;
-	a.b = a.b * b;
+	a.r *= b;
+	a.g *= b;
+	a.b *= b;
 	return (a);
 }
 
 t_color	divide_col(t_color a, int b)
 {
-	a.r = a.r / b;
-	a.g = a.g / b;
-	a.b = a.b / b;
+	a.r /= b;
+	a.g /= b;
+	a.b /= b;
 	return (a);
 }
 
 t_color	hadamard_prod(t_color a1, t_color a2)
 {
-	t_color	b;
-
-	b.r = a1.r * a2.r;
-	b.g = a1.g * a2.g;
-	b.b = a1.b * a2.b;
-	return (b);
+	a1.r *= a2.r;
+	a1.g *= a2.g;
+	a1.b *= a2.b;
+	return (a1);
 }
 
 int	col_to_int(t_color c)
