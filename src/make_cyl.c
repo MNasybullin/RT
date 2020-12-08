a5c932500afbb10766319469bfbbab25a2814a51
Milly Galt <mgalt@ga-r5.msk.21-school.ru>
2020-12-08 21:14:02 +0300

    g

--- src/make_cyl.c
+++ src/make_cyl.c
@@ -6,15 +6,38 @@
 /*   By: mgalt <mgalt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 18:36:46 by mgalt             #+#    #+#             */
-/*   Updated: 2020/12/08 20:49:27 by mgalt            ###   ########.fr       */
+/*   Updated: 2020/12/08 20:53:47 by mgalt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/rt.h"
 
-void	cone_crutch_1(t_data *p, t_world *w, char **tab, int flag)
+void	cyl_crutch_1(t_data *p, t_world *w, char **tab, int flag)
 {
-	
+	char	**tab1;
+	char	**tab2;
+	char	**tab3;
+
+	init_3_tabs(&tab1, &tab2, &tab3);
+	tab1 = ft_strsplit(tab[2], ',');
+	tab2 = ft_strsplit(tab[4], ',');
+	tab3 = ft_strsplit(tab[6], '}');
+	if (flag == 3 && !(ft_strcmp(tab[0], "obj_rotation:"))) // rotation
+	{
+		w->cyl[p->cyl_i].transform = matrix_mult(w->cyl[p->cyl_i].transform, rotation_x(ft_strtodbl(tab1[0])));
+		w->cyl[p->cyl_i].transform = matrix_mult(w->cyl[p->cyl_i].transform, rotation_y(ft_strtodbl(tab2[0])));
+		w->cyl[p->cyl_i].transform = matrix_mult(w->cyl[p->cyl_i].transform, rotation_z(ft_strtodbl(tab3[0])));
+	}
+	else if (flag == 3 && !(ft_strcmp(tab[0], "m_rotation:"))) // rotation
+	{
+		w->cyl[p->cyl_i].m.p.transform = matrix_mult(w->cyl[p->cyl_i].m.p.transform, rotation_x(ft_strtodbl(tab1[0])));
+		w->cyl[p->cyl_i].m.p.transform = matrix_mult(w->cyl[p->cyl_i].m.p.transform, rotation_y(ft_strtodbl(tab2[0])));
+		w->cyl[p->cyl_i].m.p.transform = matrix_mult(w->cyl[p->cyl_i].m.p.transform, rotation_z(ft_strtodbl(tab3[0])));
+	}
+	else if (flag == 4 && !(ft_strcmp(tab[0], "obj_scaling:"))) // scaling
+		w->cyl[p->cyl_i].transform = matrix_mult(w->cyl[p->cyl_i].transform,
+		scaling(ft_strtodbl(tab1[0]), ft_strtodbl(tab2[0]), ft_strtodbl(tab3[0])));
+	free_split_tab(tab1, tab2, tab3);
 }
 
 void	complex_params_cyl(t_data *p, t_world *w, char **tab, int flag)
@@ -37,7 +60,7 @@ void	complex_params_cyl(t_data *p, t_world *w, char **tab, int flag)
 			translation(ft_atoi(tab1[0]), ft_atoi(tab2[0]), ft_atoi(tab3[0])));
 		else if (flag == 2) // color
 			w->cyl[p->cyl_i].m.color = color(ft_strtodbl(tab1[0]), ft_strtodbl(tab2[0]), ft_strtodbl(tab3[0]));
-		else if (flag == 3 && !(ft_strcmp(tab[0], "obj_rotation:"))) // rotation
+		/*else if (flag == 3 && !(ft_strcmp(tab[0], "obj_rotation:"))) // rotation
 		{
 			w->cyl[p->cyl_i].transform = matrix_mult(w->cyl[p->cyl_i].transform, rotation_x(ft_strtodbl(tab1[0])));
 			w->cyl[p->cyl_i].transform = matrix_mult(w->cyl[p->cyl_i].transform, rotation_y(ft_strtodbl(tab2[0])));
@@ -51,8 +74,9 @@ void	complex_params_cyl(t_data *p, t_world *w, char **tab, int flag)
 		}
 		else if (flag == 4 && !(ft_strcmp(tab[0], "obj_scaling:"))) // scaling
 			w->cyl[p->cyl_i].transform = matrix_mult(w->cyl[p->cyl_i].transform,
-			scaling(ft_strtodbl(tab1[0]), ft_strtodbl(tab2[0]), ft_strtodbl(tab3[0])));
-		else if (flag == 4 && !(ft_strcmp(tab[0], "m_scaling:"))) // scaling
+			scaling(ft_strtodbl(tab1[0]), ft_strtodbl(tab2[0]), ft_strtodbl(tab3[0])));*/
+		cyl_crutch_1(p, w, tab, flag);
+		if (flag == 4 && !(ft_strcmp(tab[0], "m_scaling:"))) // scaling
 			w->cyl[p->cyl_i].m.p.transform = matrix_mult(w->cyl[p->cyl_i].m.p.transform,
 			scaling(ft_strtodbl(tab1[0]), ft_strtodbl(tab2[0]), ft_strtodbl(tab3[0])));
 		free_split_tab(tab1, tab2, tab3);
